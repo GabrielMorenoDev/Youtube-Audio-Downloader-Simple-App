ee898300b26ce7a85fa85c68041a0cac09f8c02f
GabrielDev <gabrielgodlike@gmail.com>
2024-06-08 21:05:33 -0300

    Can download video and audio separately.

--- pytube_downloader.py
+++ pytube_downloader.py
@@ -1,9 +1,30 @@
 from pytube import YouTube
+import os
+
+path = os.getcwd()
 
 def download_audio(link):
 
     youtube = YouTube(link)
 
-    stream= youtube.streams.filter(only_audio=True)
+    
 
-    stream[0].download("path")
+    audio = youtube.streams.get_audio_only()
+    
+    downloaded_audio = audio.download(path)
+
+    base, ext = os.path.splitext(downloaded_audio)
+    new_file = base + '.mp3'
+    os.rename(downloaded_audio, new_file)
+    
+    #download only video
+
+    #video = youtube.streams.filter(res='1080p').first()
+    #video.download(path)
+    
+
+
+def get_name(link):
+    youtube = YouTube(link)
+
+    return youtube.title
--- video_downloader_ui.py
+++ video_downloader_ui.py
@@ -1,10 +1,16 @@
-from pytube_downloader import download_audio
+from pytube_downloader import download_audio, get_name
 from tkinter import *
 
 def download():
     link = videoInput.get()
     download_audio(link)
+    add_to_history(get_name(link))
 
+def add_to_history(video_name):
+    with open("download_history.txt", "a") as history:
+        history.write("\n"+video_name)
+    history.close()
+    historyListBox.insert(0, video_name)
 
 
 
@@ -24,5 +30,23 @@ videoInput.pack()
 downloadButton = Button(root, text="Download Audio", padx=20, pady=20, command=download)
 downloadButton.pack()
 
+historyListBox = Listbox(root, font="Helvetica 20", height=5, width=30  , bg="gray")
+
+with open("download_history.txt", "r") as history:
+    history_list = reversed(history.readlines())
+    
+    position = 1
+    for line in history_list:
+        
+        if position != 5:
+            historyListBox.insert(position, line)
+            position += 1
+            continue
+        break
+        
+
+    history.close()
+
+historyListBox.pack()
 
 root.mainloop()
