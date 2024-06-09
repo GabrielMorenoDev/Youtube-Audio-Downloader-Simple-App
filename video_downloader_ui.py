b8a3c6a9991a4eb093d261c1569303b13eba1f5a
GabrielDev <gabrielgodlike@gmail.com>
2024-06-08 21:16:29 -0300

    creates .txt file for download history.

--- video_downloader_ui.py
+++ video_downloader_ui.py
@@ -7,10 +7,14 @@ def download():
     add_to_history(get_name(link))
 
 def add_to_history(video_name):
-    with open("download_history.txt", "a") as history:
-        history.write("\n"+video_name)
-    history.close()
-    historyListBox.insert(0, video_name)
+    try:
+        with open("download_history.txt", "a") as history:
+            history.write("\n"+video_name)
+        history.close()
+        historyListBox.insert(0, video_name)
+    except FileNotFoundError:
+        with open('log.txt', 'w') as history:
+            history.write(video_name)
 
 
 
@@ -32,20 +36,23 @@ downloadButton.pack()
 
 historyListBox = Listbox(root, font="Helvetica 20", height=5, width=30  , bg="gray")
 
+
 with open("download_history.txt", "r") as history:
     history_list = reversed(history.readlines())
-    
+        
     position = 1
     for line in history_list:
-        
+            
         if position != 5:
             historyListBox.insert(position, line)
             position += 1
             continue
         break
-        
+            
 
     history.close()
+
+    
 
 historyListBox.pack()
 
