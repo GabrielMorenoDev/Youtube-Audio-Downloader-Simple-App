031ff52834e8539576c95af6544efaddf6afe2fd
GabrielDev <gabrielgodlike@gmail.com>
2024-06-11 19:49:00 -0300

    Changed normal tkinter to tkinter bootstrap. Can now choose to download only audio, only video or both separately.

--- pytube_downloader.py
+++ pytube_downloader.py
@@ -7,6 +7,26 @@ def download_audio(link):
 
     youtube = YouTube(link)
 
+    audio = youtube.streams.get_audio_only()
+    
+    downloaded_audio = audio.download(path)
+
+    base, ext = os.path.splitext(downloaded_audio)
+    new_file = base + '.mp3'
+    os.rename(downloaded_audio, new_file)
+    
+
+def download_video(link):
+
+    youtube = YouTube(link)
+
+    video = youtube.streams.filter(res='1080p').first()
+    video.download(path)
+
+def download_audio_and_video(link):
+
+    youtube = YouTube(link)
+
     
 
     audio = youtube.streams.get_audio_only()
@@ -16,13 +36,9 @@ def download_audio(link):
     base, ext = os.path.splitext(downloaded_audio)
     new_file = base + '.mp3'
     os.rename(downloaded_audio, new_file)
-    
-    #download only video
 
-    #video = youtube.streams.filter(res='1080p').first()
-    #video.download(path)
-    
-
+    video = youtube.streams.filter(res='1080p').first()
+    video.download(path)
 
 def get_name(link):
     youtube = YouTube(link)
--- video_downloader_ui.py
+++ video_downloader_ui.py
@@ -1,9 +1,19 @@
-from pytube_downloader import download_audio, get_name
+from pytube_downloader import download_audio, get_name, download_video, download_audio_and_video
 from tkinter import *
+from ttkbootstrap.constants import *
+import ttkbootstrap as tb 
 
 def download():
     link = videoInput.get()
-    download_audio(link)
+    match option_var.get():
+        case "Audio":
+            download_audio(link)
+        case "Video":
+            download_video(link)
+        case "Both":
+            download_audio_and_video(link)
+        case _:
+            raise("Error in code")
     add_to_history(get_name(link))
 
 def add_to_history(video_name):
@@ -18,10 +28,10 @@ def add_to_history(video_name):
 
 
 
-root = Tk()
-root.minsize(250,250)
-root.configure(background="gray")
-
+#root = Tk()
+#root.minsize(250,250)
+#root.configure(background="gray")
+root = tb.Window(themename="darkly")
 
 
 
@@ -31,9 +41,18 @@ videoLabel.pack()
 videoInput = Entry(root, width=100)
 videoInput.pack()
 
-downloadButton = Button(root, text="Download Audio", padx=20, pady=20, command=download)
+downloadButton = tb.Button(root, text="Download Audio", padding=(10,10),command=download)
 downloadButton.pack()
 
+downloadOptionsButton = tb.Menubutton(text="Choose download options")
+optionsMenu = tb.Menu(downloadOptionsButton)
+option_var = StringVar(value="Audio")
+optionsMenu.add_radiobutton(label="Audio only",value="Audio",variable=option_var)
+optionsMenu.add_radiobutton(label="Video Only",value="Video",variable=option_var)
+optionsMenu.add_radiobutton(label="Both",value="Both",variable=option_var)
+downloadOptionsButton['menu'] = optionsMenu
+
+downloadOptionsButton.pack()
 historyListBox = Listbox(root, font="Helvetica 20", height=5, width=30  , bg="gray")
 
 
